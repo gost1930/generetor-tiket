80c2e2b77a93c917f34ceeaf8bb2e7a59f0cd527
gost1930 <hadbimohamed66@gmail.com>
2024-11-18 11:21:50 +0100

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import html2canvas from "html2canvas";
 function App() {
   const products = [
     { name: "IZEM CANET 33CL", price: 90 },
+    { name: "MARGARINE DIMA 250G", price: 110 },
+    { name: "MARGARINE SOL 500G", price: 180 },
+    { name: "NOULLES JUMBO", price: 50 },
+    { name: "NOULLES KAZAMI", price: 50 },
     { name: "OMO LIQUID MATIC 1L", price: 370 },
     // { name: "COUCHE CANBÉBÉ N05", price: 250 },
     // { name: "THON ISABEL 3PCS", price: 310 },
@@ -16,18 +20,12 @@ function App() {
     { name: "H'RISSA CAB", price: 180 },
     { name: "CONFITURE CAB", price: 180 },
 
-    { name: "TOUMATE IZDIHAR 400G", price: 185 },
-    { name: "H'RISSA IZDIHAR", price: 185 },
-    { name: "CONFITURE IZDIHAR", price: 185 },
-
+    
     { name: "MARGARINE DIMA 500G", price: 185 },
-    { name: "MARGARINE DIMA 250G", price: 110 },
-    { name: "MARGARINE SOL 500G", price: 180 },
-    { name: "NOULLES JUMBO", price: 50 },
-    { name: "NOULLES KAZAMI", price: 50 },
     { name: "THON RICAMAR 3PCS", price: 310 },
     { name: "THON MARATUN 3PCS", price: 310 },
     // { name: "SIDI SAADA CAMEMBRET", price: 380 },
+    { name: "BIMO COOKIES", price: 100 },
     { name: "TASSILI CAMEMEBRET", price: 360 },
 
     // { name: "FROMAGE TARTINO JUNIOR 16PCS", price: 230 },
@@ -36,124 +34,127 @@ function App() {
     // { name: "FROMAGE LA VACHE QUI RIT 24PCS", price: 270 },
 
     { name: "BIMO MACAO", price: 100 },
+    { name: "TOUMATE IZDIHAR 400G", price: 185 },
+    { name: "H'RISSA IZDIHAR", price: 185 },
+    { name: "JUS IFRUIT 1L", price: 190 },
+    { name: "CONFITURE IZDIHAR", price: 185 },
     { name: "BIMO BISCUITS", price: 100 },
-    { name: "BIMO COOKIES", price: 100 },
-    { name: "BIMO GAUFRETES", price: 100 },
     { name: "BIMO CHOKOLAT", price: 100 },
-
+    
     // { name: "MAXON BISCUIT XL", price: 230 },
-
+    
     // { name: "EAU LALA KHEDIDJA 1.5L", price: 45 },
 
     // { name: "EAU GUEDILA 1.5L", price: 45 },
-
-    { name: "HAMOUD SELECTO 2L", price: 140 },
-    { name: "JUS RAMY 1.25L", price: 135 },
-    { name: "RAMY CANETTE 33CL", price: 135 },
-
+    
+    
     { name: "JUS IFRUIT 1L", price: 110 },
-    { name: "JUS IFRUIT 1L", price: 190 },
-
+    
     // { name: "SOUMMAME L'BEN 1L", price: 145 },
-
+    
     // { name: "HODNA L'BEN 1L", price: 145 },
-
     // { name: "CANDIA L'BEN 1L", price: 145 },
 
-    { name: "SOUMMAME LAIT 1L", price: 130 },
     { name: "HODNA LAIT 1L", price: 110 },
-
-    // { name: "CANDY CHOCO 1L ", price: 190 },
-
+    
+    { name: "SOUMMAME LAIT 1L", price: 130 },
+    { name: "JUS RAMY 1.25L", price: 135 },
+    { name: "RAMY CANETTE 33CL", price: 135 },
+    
     // { name: "JAVEL LIFE 5L", price: 230 },
-
-    { name: "BINGO GEL MACHINE 1L ", price: 370 },
-    { name: "BINGO ASSOUPLISSANT ", price: 370 },
+    // { name: "CANDY CHOCO 1L ", price: 190 },
+    
     { name: "BINGO LIQUIDE VAISSELLE ", price: 370 },
-
+    { name: "BIMO GAUFRETES", price: 100 },
+    
+    { name: "BINGO ASSOUPLISSANT ", price: 370 },
+    { name: "HAMOUD SELECTO 2L", price: 140 },
+    
+    
     // { name: "FORCE XPERSS LAVE SOL 1L", price: 175 },
-
+    { name: "AIGLE LIQUIDE VAISSELLE 970ML", price: 170 },
     // { name: "BRILEX LAVE SOL 1L", price: 165 },
-
-    { name: "AMIR CLEAN LAVE SOL 1L", price: 175 },
-    { name: "AMIR GEL MACHINE 2.5L", price: 730 },
+    
     { name: "AMIR LIQUIDE VAISSELLE 750ML", price: 175 },
     { name: "AMIR SAVON LIQUIDE", price: 125 },
-
+    
     { name: "AIGLE GEL MACHINE 1L", price: 330 },
     { name: "AIGLE LAVE SOL 1L", price: 150 },
-    { name: "AIGLE LIQUIDE VAISSELLE 970ML", price: 170 },
     { name: "AIGLE SAVON LIQUIDE", price: 120 },
 
+    
     // { name: "ISIS SACHET 300G", price: 95 },
 
     // { name: "TEST JAVEL 1L", price: 80 },
 
+    { name: "BINGO GEL MACHINE 1L ", price: 370 },
     { name: "CELIA DEVELOP 1 400G", price: 750 },
     { name: "CELIA DEVELOP 2 500G", price: 750 },
-
+    
     // { name: "BLÉDILAIT NURSIE 1 400G", price: 720 },
-
-    { name: "COUCHE BIMBIES N01", price: 260 },
-    { name: "COUCHE BIMBIES N02", price: 260 },
     { name: "COUCHE BIMBIES N03", price: 260 },
+    { name: "COUCHE BIMBIES N01", price: 260 },
+    { name: "AMIR GEL MACHINE 2.5L", price: 730 },
+
     { name: "COUCHE BIMBIES N04", price: 260 },
-    { name: "COUCHE BIMBIES N05", price: 260 },
-    { name: "COUCHE BIMBIES N06", price: 260 },
-
-    { name: "COUCHE MOLFIX N01", price: 260 },
-    { name: "COUCHE MOLFIX N02", price: 260 },
-    { name: "COUCHE MOLFIX N03", price: 260 },
-    { name: "COUCHE MOLFIX N04", price: 260 },
-    { name: "COUCHE MOLFIX N05", price: 260 },
     { name: "COUCHE MOLFIX N06", price: 260 },
-
+    { name: "COUCHE BIMBIES N02", price: 260 },
     { name: "SHAMPOOING VENUS 2EN1", price: 120 },
-
+    
     { name: "DÉODORANT FLUX CARE", price: 250 },
-
-    { name: "CHIPS MAHBOL", price: 60 },
-
-    // { name: "JUS ROUIBA 1L", price: 110 },
-
-    // { name: "CAFÉ MANY 250G", price: 380 },
-
-    // { name: "EAU IFRI 1.5L", price: 45 },
-
-    { name: "COCA COLA 1.5L", price: 130 },
-
-    // { name: "OMO LIQUID MATIC 2.5L", price: 810 },
-
-    { name: "YAOURT DANETTE MAXY", price: 55 },
-
-    { name: "CHOCOLAT MOMENT NOIR", price: 210 },
-    { name: "KOOL 4 WINNERS", price: 60 },
-
+    { name: "LESIEUR HUILE", price: 10 },
+    { name: "CHEEZY PORSION", price: 10 },
+    { name: "COUCHE BIMBIES N06", price: 260 },
+    
+    { name: "COUCHE MOLFIX N02", price: 260 },
     { name: "YAOURT SOUMMAM FORT", price: 25 },
+    { name: "COUCHE MOLFIX N01", price: 260 },
+    { name: "COUCHE MOLFIX N05", price: 260 },
+    { name: "AMIR CLEAN LAVE SOL 1L", price: 175 },
+    { name: "KOOL 4 WINNERS", price: 60 },
+    
+    { name: "COUCHE BIMBIES N05", price: 260 },
+    { name: "CHIPS MAHBOL", price: 60 },
+    
+    // { name: "JUS ROUIBA 1L", price: 110 },
+    
+    // { name: "CAFÉ MANY 250G", price: 380 },
+    
+    // { name: "EAU IFRI 1.5L", price: 45 },
+    
+    { name: "COCA COLA 1.5L", price: 130 },
+    
+    // { name: "OMO LIQUID MATIC 2.5L", price: 810 },
+    
+    { name: "YAOURT DANETTE MAXY", price: 55 },
+    { name: "DULCESOL COMMPLET ET MINI", price: 200 },
+    
+    { name: "DULCESOL SNACK TRUFFE FAMILIAL", price: 240 },
+    
 
     // { name: "LAIT GLORIA JUNIOR 500G", price: 670 },
-
+    { name: "COUCHE MOLFIX N04", price: 260 },
+    
     { name: "OMO LIQuid matic 1L ", price: 370 },
-
-    { name: "DULCESOL SNACK TRUFFE FAMILIAL", price: 240 },
-
-    { name: "DULCESOL BURGER", price: 200 },
+    
+    { name: "COUCHE MOLFIX N03", price: 260 },
     { name: "DULCESOL CLASSIQUE BLEUE", price: 200 },
-    { name: "DULCESOL COMMPLET ET MINI", price: 200 },
+    { name: "MASTER CHIPS", price: 120 },
     { name: "DULCESOL MADELEINE", price: 180 },
-
+    { name: "DULCESOL PANDORINO ", price: 85 },
+    { name: "CHOCOLAT MOMENT NOIR", price: 210 },
+    
     // { name: "NOUILLES YAOMY", price: 70 },
-
+    
+    { name: "DULCESOL BURGER", price: 200 },
     // { name: "GLACE GINI MINI", price: 60 },
 
     { name: "GLACE PRIMA", price: 70 },
 
-    { name: "MARGARINE FLEURIAL 500G", price: 185 },
 
     // { name: "QAADA BISCUITS", price: 130 },
 
-    { name: "MASTER CHIPS", price: 120 },
-
+    
     // { name: "LAIT NAN 400G", price: 770 },
 
     // { name: "LAIT GLORIA JUNIOR 125G ", price: 200 },
@@ -161,10 +162,10 @@ function App() {
     // { name: "LAIT GLORIA JUNIOR 350G", price: 680 },
     { name: "MADELEINE DULCESOL  FRAISE ET ABRICOT", price: 160 },
 
-    { name: "DULCESOL PANDORINO ", price: 85 },
 
     // { name: "PRÉSIDENT CAMEMBERT", price: 380 },
 
+    { name: "MARGARINE FLEURIAL 500G", price: 185 },
     { name: "CAFÉ 1001", price: 390 },
 
     // { name: "CAFÉ AROMA", price: 380 },
@@ -173,17 +174,14 @@ function App() {
 
     { name: "CHIPS MAHBOUL FRITOS", price: 10 },
 
-    { name: "LESIEUR HUILE", price: 10 },
-    { name: "LESIEUR MAYONNAISE", price: 10 },
     { name: "LESIEUR SAUCE", price: 10 },
     { name: "LESIEUR SAUCE", price: 10 },
     { name: "CHIPS MAHBOUL FRITOS", price: 10 },
+    { name: "LESIEUR MAYONNAISE", price: 10 },
 
-    { name: "CHEEZY PORSION", price: 10 },
 
   ];
 
-  console.log(products.length);
   const [userName, setUserName] = useState("");
   const [logIn, setLogIn] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -195,9 +193,12 @@ function App() {
   const [placeName, setPlaceName] = useState("");
   const [cashierName, setCashierName] = useState("");
 
+  const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
   // تحقق من تسجيل الدخول
   const isUserLoggedIn = () => {
-    if (userName === "1") {
+    if (userName === "19") {
       setLogIn(true);
       setCurrentIndex(0); // إعادة تعيين الفهرس عند تسجيل الدخول
     } else {
